6068100c2ccb181c6a659050be959d241ecbb6bc
irony <hellgirlfan97@gmail.com>
2022-03-20 03:11:09 -0400

    platform selection in env

--- src/mongo_conn.rs
+++ src/mongo_conn.rs
@@ -1,10 +1,27 @@
+use std::env;
+
 use mongodb::options::{ClientOptions, CollectionOptions, ResolverConfig};
 use mongodb::*;
 pub async fn get_mongo_client(connection_str: &str) -> mongodb::error::Result<Client> {
-    let client_options =
-        ClientOptions::parse_with_resolver_config(connection_str, ResolverConfig::cloudflare())
-            .await?;
-    Client::with_options(client_options)
+    let platform = env::var("PLATFORM").expect("No PLATFORM env set.");
+
+    match platform.as_str() {
+        "windows" => {
+            let client_options =
+                ClientOptions::parse_with_resolver_config(connection_str, ResolverConfig::cloudflare())
+                    .await?;
+            Client::with_options(client_options)
+        }
+        "linux" => {
+            let client_options =
+                ClientOptions::parse(connection_str)
+                    .await?;
+            Client::with_options(client_options)
+        }
+        _ => {
+            panic!("Invalid PLATFORM env value.")
+        }
+    }
 }
 
 pub async fn get_db(client: &Client, db_name: &str) -> Database {
